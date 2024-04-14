1a5553e5c683bc366335c78a99feebaf65aa3c17
Peter Sideris <petersid2022@gmail.com>
2024-04-14 15:37:20 +0300

    fixed rename script

--- data/rename.py
+++ data/rename.py
@@ -8,11 +8,16 @@ if __name__ == "__main__":
     path = "./IMG"
 
     for root, _, files in os.walk(path):
-        for i, filename in enumerate(files, start=1):
+        files_sorted = sorted(
+            files, key=lambda f: os.path.getmtime(os.path.join(root, f))
+        )
+
+        for i, filename in enumerate(files_sorted, start=1):
             current_file = os.path.join(root, filename)
             new_file = os.path.join(root, str(i)) + ".jpg"
 
             os.rename(current_file, new_file)
+            # print(new_file)
 
     columns = ["Center", "Left", "Right", "Steering", "Throttle", "Brake", "Speed"]
     data = read_csv("driving_log.csv", names=columns)
